c52e0d43a2c06ab7b702c4c2816a5b2705145eda
difficultcomplexity <19alex88kuzich@gmail.com>
2022-01-16 18:34:38 +0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -18,9 +18,9 @@ let VERSION = {
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0g</h3><br>
-		- G.<br>
-		- F.`
+	<h3>v0.1g</h3><br>
+		- Added 2 Weight Buffs.<br>
+		- Balanced up to 1000 G.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
