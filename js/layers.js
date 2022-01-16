88eb65d512460221e134ad2ea13161d09e00a404
difficultcomplexity <19alex88kuzich@gmail.com>
2022-01-16 17:52:47 +0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,11 @@
 addLayer("w", {
+    upgrades: {
+        11: {
+            title: "Make this whatever you want!",
+            description: "Double your point gain.",
+            cost: new Decimal(1),
+         }
+    },
     name: "weight", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "w", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
