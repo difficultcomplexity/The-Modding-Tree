c645f14f005f435d114b41b82faa1d0ac35d0713
difficultcomplexity <19alex88kuzich@gmail.com>
2022-01-21 18:22:41 +0300

    Update comparison.js

--- js/comparison.js
+++ js/comparison.js
@@ -12,75 +12,98 @@ addLayer("c", {
             done() { return player.c.points.gte(10) }
         },
         3: {
-            requirementDescription: "50 Comparisons",
+            requirementDescription: "100 Comparisons",
             effectDescription: "Unlock Earth Challenge.",
             done() { return player.c.points.gte(50) }
         },
         4: {
-            requirementDescription: "200 Comparisons",
+            requirementDescription: "1000 Comparisons",
             effectDescription: "Unlock Sun Challenge.",
             done() { return player.c.points.gte(200) }
         },
         5: {
-            requirementDescription: "500 Comparisons",
+            requirementDescription: "10000 Comparisons",
             effectDescription: "Unlock Milky Way Challenge.",
             done() { return player.c.points.gte(500) }
         },
         6: {
-            requirementDescription: "1000 Comparisons",
+            requirementDescription: "100000 Comparisons",
             effectDescription: "Unlock Universe Challenge.",
+            done() { return player.c.points.gte(1000) }
+        },
+        7: {
+            requirementDescription: "1e10 Comparisons",
+            effectDescription: "Unlock 2 Challenges.",
             done() { return player.c.points.gte(1000) }
         }
     },
     challenges: {
         11: {
             name: "Mountaintop: Volcanic",
-            challengeDescription: "Upgrade W12 is NOT boosting your grams.",
+            challengeDescription: "Upgrade W12 is useless and reduce gram gain to ^0.6.",
             goalDescription: "1e15 Grams",
             rewardDescription: "Comparisons increase weight gain by ^0.4.",
             canComplete: function() {return player.points.gte(1e15)},
+            unlocked() { return (hasMilestone('c', 1)) },
+            
         },
         12: {
             name: "Mountain Evertop",
-            challengeDescription: "Upgrade W13 and W14 is NOT boosting your grams.",
+            challengeDescription: "Upgrade W13,14 is useless and reduce gram gain to ^0.75.",
             goalDescription: "1e20 Grams",
             rewardDescription: "You get more grams based on formula: (log5(Comparisons^2)^2)x.",
             canComplete: function() {return player.points.gte(1e20)},
+            unlocked() { return (hasMilestone('c', 2)) },
         },
         21: {
             name: "Moon",
-            challengeDescription: "Upgrade O11 and W12 is NOT boosting your grams.",
+            challengeDescription: "Upgrade O11 and W12 is useless and divide gram gain by 1e6.",
             goalDescription: "1e25 Grams",
             rewardDescription: "Unlock 2 new upgrades.",
             canComplete: function() {return player.points.gte(1e25)},
+            unlocked() { return (hasMilestone('c', 3)) },
         },
         22: {
             name: "Betelgeuse",
-            challengeDescription: "Upgrade O11,12,14 is NOT boosting your grams.",
+            challengeDescription: "Upgrade O11,12,14 is useless and reduce gram gain to ^0.8.",
             goalDescription: "1e30 Grams",
             rewardDescription: "Unlock a buyable.",
             canComplete: function() {return player.points.gte(1e30)},
+            unlocked() { return (hasMilestone('c', 4)) },
         },
         31: {
             name: "Choco Way",
-            challengeDescription: "Upgrade O14 and W12 is NOT boosting your grams.",
+            challengeDescription: "Upgrade W12 is useless and reduce gram gain to ^0.5",
             goalDescription: "1e40 Grams",
             rewardDescription: "Unlock another buyable.",
             canComplete: function() {return player.points.gte(1e40)},
+            unlocked() { return (hasMilestone('c', 5)) },
         },
         32: {
             name: "The Edge",
-            challengeDescription: "Upgrade W13,14,15 is NOT boosting your grams.",
+            challengeDescription: "Upgrade W13,14,15 is useless.",
             goalDescription: "1e50 Grams",
-            rewardDescription: "You are worthy, now. Completing this will grant you time travelling.",
+            rewardDescription: "You are worthy, now. Completing this will grant you a new layer.",
             canComplete: function() {return player.points.gte(1e50)},
+            unlocked() { return (hasMilestone('c', 6)) },
         },
         41: {
             name: "The Shattered Universe",
-            challengeDescription: "Upgrade W12,13,14,15 and O11,14,15 is NOT boosting your grams.",
+            challengeDescription: "Upgrade W12,13,14,15 and O15 is useless and reduce gram gain to ^0.1",
             goalDescription: "Googol Grams",
             rewardDescription: "You are worthy, now. Completing this will grant you time travelling.",
             canComplete: function() {return player.points.gte(1e100)},
+            unlocked() { return (hasMilestone('c', 4)) },
+
+        },
+        42: {
+            name: "MegaDeath",
+            challengeDescription: "Reduce gram gain to ^0.002.",
+            goalDescription: "1e15 Grams",
+            rewardDescription: "^1.1 to all below currencies.",
+            canComplete: function() {return player.points.gte(1e15)},
+            unlocked() { return (hasMilestone('c', 7)) },
+
         },
     },
     name: "comparison", // This is optional, only used in a few places, If absent it just uses the layer id.
